a88856f0d8915fd3af067d9ed3971f3d47423703
jacob leveroni <jacobjleveroni.dev@gmail.com>
2025-12-11 10:45:13 -0800

    added animated player and rudimentary iso movement

--- game/scripts/player/player.gd
+++ game/scripts/player/player.gd
@@ -1,44 +1,92 @@
-extends CharacterBody2D
+extends CharacterBody2D;
 
-@export var agent: NavigationAgent2D;
-@export var anim_tree: AnimationTree;
-@export var speed: float = 8;
+@export var speed = 300;
 
-var state_machine;
-var follow_mouse: bool = false;
+var current_direction
 
-func _ready():
-	state_machine = anim_tree.get("parameters/playback");
-	
-func _unhandled_input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == MouseButton.MOUSE_BUTTON_RIGHT:
-			follow_mouse = event.pressed;
+enum DIRECTION {
+	UP,
+	UP_LEFT,
+	UP_RIGHT,
+	DOWN,
+	DOWN_LEFT,
+	DOWN_RIGHT,
+	LEFT,
+	RIGHT,
+	IDLE
+};
 
-func _physics_process(_delta):
-	if follow_mouse:
-		var click_position = get_global_mouse_position();
-		agent.target_position = click_position;
+var KEY_UP = false;
+var KEY_DOWN = false;
+var KEY_LEFT = false;
+var KEY_RIGHT = false;
 
-	if agent.is_navigation_finished():
-		velocity = Vector2.ZERO;
-		state_machine.travel("Idle");
-		return;
-		
-	var next_position = agent.get_next_path_position();
-	var direction = (next_position - global_position).normalized();
-	
-	velocity = direction * speed;
-	move_and_slide();
-	_update_animation(direction);
-	
-	
-func _update_animation(direction: Vector2):
-	anim_tree.set("parameters/Idle/blend_position", direction);
-	anim_tree.set("parameters/Run/blend_position", direction);
-	
-	if direction.length() > 0.1:
-		state_machine.travel("Run");
+# used to map up/down/left/right to isometric compatible directions so we can just pass normal catesian coords for our diagonal movement
+func cartesian_to_isometric(cartesian: Vector2):
+	return Vector2(cartesian.x - cartesian.y, (cartesian.x + cartesian.y) / 2);
+
+func _process(_delta: float) -> void:
+	set_inputs();
+	set_direction();
+	move();
+
+
+func set_direction():
+	if KEY_UP:
+		if KEY_LEFT:
+			current_direction = DIRECTION.UP_LEFT;
+		elif KEY_RIGHT:
+			current_direction = DIRECTION.UP_RIGHT;
+		else:
+			current_direction = DIRECTION.UP;
+	elif KEY_DOWN:
+		if KEY_LEFT:
+			current_direction = DIRECTION.DOWN_LEFT;
+		elif KEY_RIGHT:
+			current_direction = DIRECTION.DOWN_RIGHT;
+		else:
+			current_direction = DIRECTION.DOWN;
+	elif KEY_LEFT:
+		current_direction = DIRECTION.LEFT;
+	elif KEY_RIGHT:
+		current_direction = DIRECTION.RIGHT;
 	else:
-		state_machine.travel("Idle");
-	
+		current_direction = DIRECTION.IDLE;
+
+func set_inputs():
+	KEY_UP = Input.is_action_pressed("move_up");
+	KEY_DOWN = Input.is_action_pressed("move_down");
+	KEY_LEFT = Input.is_action_pressed("move_left");
+	KEY_RIGHT = Input.is_action_pressed("move_right");
+
+func move():
+	match current_direction:
+		DIRECTION.UP:
+			self.velocity = Vector2(0, -speed);
+			$AnimatedSprite2D.play("up");
+		DIRECTION.UP_LEFT:
+			self.velocity = cartesian_to_isometric(Vector2(-speed, 0));
+			$AnimatedSprite2D.play("up_left");
+		DIRECTION.UP_RIGHT:
+			self.velocity = cartesian_to_isometric(Vector2(0, -speed));
+			$AnimatedSprite2D.play("up_right");
+		DIRECTION.DOWN:
+			self.velocity = Vector2(0, speed);
+			$AnimatedSprite2D.play("down");
+		DIRECTION.DOWN_LEFT:
+			self.velocity = cartesian_to_isometric(Vector2(0, speed));
+			$AnimatedSprite2D.play("down_left");
+		DIRECTION.DOWN_RIGHT:
+			self.velocity = cartesian_to_isometric(Vector2(speed, 0));
+			$AnimatedSprite2D.play("down_right");
+		DIRECTION.LEFT:
+			self.velocity = Vector2(1 * -speed, 0);
+			$AnimatedSprite2D.play("left");
+		DIRECTION.RIGHT:
+			self.velocity = Vector2(-1 * -speed, 0);
+			$AnimatedSprite2D.play("right");
+		DIRECTION.IDLE:
+			self.velocity = Vector2.ZERO;
+			$AnimatedSprite2D.play("idle");
+
+	move_and_slide();
